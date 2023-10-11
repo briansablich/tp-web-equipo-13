e69acddb62c221ef2247c58ca0f2f5e8122afda3
matinicotra <matiasanicotra@gmail.com>
2023-10-11 14:34:24 -0300

    parte del desarrollo de cargar img para detalle articulo

--- WebCatalogo/detalleArticulo.aspx
+++ WebCatalogo/detalleArticulo.aspx
@@ -7,4 +7,29 @@
 
 
 
+    <div id="carouselExample" class="carousel slide">
+
+        <div class="carousel-inner">
+            <asp:Repeater ID="repCarouselImagenes" runat="server">
+                <ItemTemplate>
+
+                    <div class="carousel-item active">
+                        <img src="#" class="d-block w-100" alt="...">
+                    </div>
+
+                </ItemTemplate>
+            </asp:Repeater>
+        </div>
+
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+
 </asp:Content>
--- WebCatalogo/detalleArticulo.aspx.cs
+++ WebCatalogo/detalleArticulo.aspx.cs
@@ -12,24 +12,25 @@ namespace WebCatalogo
     public partial class detalleArticulo : System.Web.UI.Page
     {
         public string idArticuloUrl { get; set; }
+
         public List<Imagen> ListaImagenes = new List<Imagen>();
+        public List<Imagen> ImgsDelArticulo = new List<Imagen>();
 
         protected void Page_Load(object sender, EventArgs e)
         {
             ImagenNegocio imagenNegocio = new ImagenNegocio();
             ListaImagenes = imagenNegocio.ObtenerDatos();
 
-            idArticuloUrl = Request.QueryString["id"];         // captura el id del articulo a mostrar detalle, que llega por url 
+
+            //DESARROLLO PARA AGRUPAR LAS IMG DEL ARTICULO...
+
+            foreach (Dominio.Imagen img in ListaImagenes)
+            {
+                if (img.IdImagen == Convert.ToInt32(Request.QueryString["id"])) 
+                    ImgsDelArticulo.Add(img);
+            }
 
 
-            //desarrollo para agrupar las imagenes del articulo
-
-            //List<Imagen> imgDeArticulo = new List<Imagen>();
-
-            //foreach (Dominio.Imagen img in ListaImagenes)
-            //{
-            //    if (img.IdImagen == (int)Request.QueryString["id"]) imgDeArticulo.Add(IdImagen);
-            //}
         }
     }
 }
